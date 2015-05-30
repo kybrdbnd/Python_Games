a0ac45d0adf40e423fd822f41a56a0169cf9abda
Pranav Puri <pranavpuri.p@hotmail.com>
2015-05-30 15:41:55 +0530

    just another one

--- login.py
+++ login.py
@@ -18,17 +18,17 @@ except:
     print('its already created')
 
 # all variables
-logingui = Tk() #object of Tkinter for login
-#varibles for signin
+logingui = Tk()  # object of Tkinter for login
+# varibles for signin
 email_entry = StringVar()
 username_entry = StringVar()
 password_entry = StringVar()
-#varibles for signup
+# varibles for signup
 emaile = StringVar()
 usernamee = StringVar()
 passworde = StringVar()
 cpassword = StringVar()
-#varibles to control block
+# varibles to control block
 forgotblock = 1
 signupblock = 1
 
@@ -78,13 +78,14 @@ def signin():
     signupblock = 0
     return
 
+
 # method for signup
 def make_id():
     global emaile
     global usernamee
     global passworde
     global cpassword
-    
+
     b = conn.cursor()
     userexist = emailexist = 0
     emailvalidation = 1
@@ -93,26 +94,26 @@ def make_id():
     if len(str(*cursor)):
         userexist = 1
     cursor1 = b.execute(
-    "SELECT email FROM database where email = '%s'" % emaile.get())
+        "SELECT email FROM database where email = '%s'" % emaile.get())
     if len(str(*cursor1)):
         emailexist = 1
     domains = ['yahoo.com', 'gmail.com', 'hotmail.com', 'outlook.com']
     email = emaile.get()
     find = email.find('@')
     email = email[find + 1:]
-# checking the validation of an email id 
+    # checking the validation of an email id
     if email not in domains:
         messagebox.showerror("Games on Python", "Enter a valid Email Id")
         emailvalidation = 0
-    if len(emaile.get()) == 0 or len(usernamee.get()) == 0 : 
+    if len(emaile.get()) == 0 or len(usernamee.get()) == 0:
         messagebox.showwarning("Warning", "Fields cannot be left blank!!!")
     else:
         if emailexist or userexist:
             messagebox.showwarning("Warning", "Email or Username exists")
-        elif emailvalidation == 1 and cpassword.get()==passworde.get():
+        elif emailvalidation == 1 and cpassword.get() == passworde.get():
             messagebox.showinfo("Success", "User Created Successfully!!!")
             c.execute("INSERT INTO database VALUES ('%s','%s','%s')" %
-              (emaile.get(), usernamee.get(), passworde.get()))
+                      (emaile.get(), usernamee.get(), passworde.get()))
             conn.commit()
 
     global forgotblock
@@ -121,12 +122,14 @@ def make_id():
     signupblock = 0
 
     return
+
+
 # method for forgot username
 def forgot():
     global newusername_entry
     global newpassword_entry
     global confirmpassword
-    
+
     usernamee = username_entry
     passworde = newpassword_entry
     cpassworde = confirmpassword
@@ -136,7 +139,7 @@ def forgot():
         "SELECT username FROM database where username ='%s'" % usernamee.get())
     cursor1 = b.execute(
         "SELECT password FROM database where password='%s'" % passworde.get())
-    print(passworde.get(), usernamee.get(),cpassworde.get())
+    print(passworde.get(), usernamee.get(), cpassworde.get())
     if (len(usernamee.get()) == 0 or len(passworde.get()) == 0):
         messagebox.showwarning("Warning", "Fields cannot be left blank")
     else:
@@ -156,6 +159,7 @@ def forgot():
             messagebox.showerror("Games on Python", "Username not found")
     return
 
+
 def forgetdestroy():
     global forgotblock
     global signupblock
@@ -164,17 +168,21 @@ def forgetdestroy():
     logingui.destroy()
     return
 
+
 def signupdestroy():
     global forgotblock
     global signupblock
     forgotblock = 0
     signupblock = 1
     logingui.destroy()
-    return 
+    return
+
+
 def again():
     signup.destroy()
     os.system("python3 combined.py")
     return
+
 
 def again1():
     mforget.destroy()
@@ -186,7 +194,7 @@ logingui.title("Mini Games For Python")
 
 # login Panel
 image = PhotoImage(file=r"login.png")
-Label(logingui, image=image).pack(side="right",padx=30,pady=50)
+Label(logingui, image=image).pack(side="right", padx=30, pady=50)
 loginpanel = Label(
     logingui, text="Login Panel", font=("", 25)).place(x=145, y=10)
 
@@ -195,9 +203,8 @@ userentry = Entry(logingui, textvariable=username_entry).place(
     x=140, y=83, width=200)
 
 passwordlabel = Label(logingui, text="Password:", font=("", 15)).place(x=30, y=130)
-passwordentry = Entry(logingui, textvariable=password_entry,show="*").place(
+passwordentry = Entry(logingui, textvariable=password_entry, show="*").place(
     x=140, y=133, width=200)
-
 
 signinbutton = Button(
     logingui, text="SIGN IN", font=("", 10), command=signin).place(x=80, y=190)
@@ -212,8 +219,8 @@ forgotbutton = Button(logingui, text="CLICK HERE", font=(
 exitbutton = Button(text="EXIT", font=("", 8), command=exit).place(x=440, y=0)
 logingui.mainloop()
 
-#signup panel  
-if  signupblock and forgotblock == 0:
+# signup panel
+if signupblock and forgotblock == 0:
     signup = Tk()
     signup.geometry("500x300+200+300")
     signup.title("Signup for Games")
@@ -222,28 +229,28 @@ if  signupblock and forgotblock == 0:
     passworde = StringVar()
     cpassword = StringVar()
 
-    Label(signup, text="Email ID:",font=("",12)).place(x=10,y=10)
-    Entry(signup, textvariable = emaile).place(x=220,y=10)
+    Label(signup, text="Email ID:", font=("", 12)).place(x=10, y=10)
+    Entry(signup, textvariable=emaile).place(x=220, y=10)
 
-    Label(signup, text="Username:", font=("",12)).place(x=10,y=60)
-    Entry(signup, textvariable = usernamee).place(x=220,y=60)
+    Label(signup, text="Username:", font=("", 12)).place(x=10, y=60)
+    Entry(signup, textvariable=usernamee).place(x=220, y=60)
 
-    Label(signup, text="Password:", font=("",12)).place(x=10,y=110)
-    Entry(signup, textvariable = passworde, show="*").place(x=220,y=110)
+    Label(signup, text="Password:", font=("", 12)).place(x=10, y=110)
+    Entry(signup, textvariable=passworde, show="*").place(x=220, y=110)
 
-    Label(signup, text="Confirm Password:", font=("",12)).place(x=10,y=160)
-    Entry(signup, textvariable = cpassword, show="*").place(x=220,y=160)
+    Label(signup, text="Confirm Password:", font=("", 12)).place(x=10, y=160)
+    Entry(signup, textvariable=cpassword, show="*").place(x=220, y=160)
 
-    Button(signup, text="OK",font=("",10),command=make_id).place(x=170,y=210)
-    Button(signup ,text="Click Here to login again",font=("",10),
-            command=again).place(x=110,y=250)
+    Button(signup, text="OK", font=("", 10), command=make_id).place(x=170, y=210)
+    Button(signup, text="Click Here to login again", font=("", 10),
+           command=again).place(x=110, y=250)
     signup.mainloop()
 
 
 
 # forgot Panel
-if forgotblock and signupblock == 0: # check for the signup or signin button 
-    mforget = Tk() # object of Tkinter module for forget panel
+if forgotblock and signupblock == 0:  # check for the signup or signin button
+    mforget = Tk()  # object of Tkinter module for forget panel
     username_entry = StringVar()
     newpassword_entry = StringVar()
     confirmpassword = StringVar()
@@ -262,16 +269,16 @@ if forgotblock and signupblock == 0: # check for the signup or signin button
         x=200, y=80, width=200)
 
     cpasslabel = Label(
-        mforget, text = "Confirm Password:", font=("",12)).place(x=10,y=130)
+        mforget, text="Confirm Password:", font=("", 12)).place(x=10, y=130)
     cpassentry = Entry(mforget, textvariable=confirmpassword).place(
-        x=200, y=130,width = 200)
+        x=200, y=130, width=200)
 
     forgetbutton = Button(mforget, text="OK", font=("", 10), command=forgot).place(
-        x=100, y=160,width= 50)
+        x=100, y=160, width=50)
     exitbutton1 = Button(
         mforget, text="Exit", font=("", 8), command=exit).place(x=425, y=0)
-    Button(mforget ,text="Click Here to login again",font=("",10),
-            command=again1).place(x=160,y=160)
+    Button(mforget, text="Click Here to login again", font=("", 10),
+           command=again1).place(x=160, y=160)
     global signupblock
     signupblock = 0
     mforget.mainloop()
